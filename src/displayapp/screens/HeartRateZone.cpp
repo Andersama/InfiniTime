134efa0a80c5b2fe17458a8d7a09cd79ce1ecaa3
Andersama <anderson.john.alexander@gmail.com>
2025-12-08 12:57:49 -0800

    Fixup progress bar

--- src/displayapp/screens/HeartRateZone.cpp
+++ src/displayapp/screens/HeartRateZone.cpp
@@ -19,21 +19,21 @@ HeartRateZone::HeartRateZone(Controllers::HeartRateController& heartRateControll
 
   lv_obj_t* screen = lv_scr_act();
 
-  title = lv_label_create(screen);
+  title = lv_label_create(screen, nullptr);
   lv_label_set_text_static(title, "BPM Breakdown");
   lv_obj_align(title, screen, LV_ALIGN_IN_TOP_MID, 0, 20);
 
   total_bar = lv_bar_create(screen, nullptr);
-  lv_obj_set_style_local_bg_opa(zone_bar[i], LV_BAR_PART_BG, LV_STATE_DEFAULT, LV_OPA_0);
-  lv_obj_set_style_local_line_color(zone_bar[i], LV_BAR_PART_BG, LV_STATE_DEFAULT, Colors::bgAlt);
-  lv_obj_set_style_local_border_width(zone_bar[i], LV_BAR_PART_BG, LV_STATE_DEFAULT, 2);
-  lv_obj_set_style_local_radius(zone_bar[i], LV_BAR_PART_BG, LV_STATE_DEFAULT, 0);
-  lv_obj_set_style_local_line_color(zone_bar[0], LV_BAR_PART_INDIC, LV_STATE_DEFAULT, LV_COLOR_NAVY);
+  lv_obj_set_style_local_bg_opa(total_bar, LV_BAR_PART_BG, LV_STATE_DEFAULT, LV_OPA_0);
+  lv_obj_set_style_local_line_color(total_bar, LV_BAR_PART_BG, LV_STATE_DEFAULT, Colors::bgAlt);
+  lv_obj_set_style_local_border_width(total_bar, LV_BAR_PART_BG, LV_STATE_DEFAULT, 2);
+  lv_obj_set_style_local_radius(total_bar, LV_BAR_PART_BG, LV_STATE_DEFAULT, 0);
+  lv_obj_set_style_local_line_color(total_bar, LV_BAR_PART_INDIC, LV_STATE_DEFAULT, LV_COLOR_NAVY);
 
   lv_obj_align(total_bar, screen, LV_ALIGN_IN_TOP_MID, 0, offset - (zone_bar.size() * 25));
 
-  total_label = lv_label_create(zone_bar[i], nullptr);
-  lv_obj_align(total_label, zone_bar[i], LV_ALIGN_CENTER, 0, 0);
+  total_label = lv_label_create(total_bar, nullptr);
+  lv_obj_align(total_label, total_bar, LV_ALIGN_CENTER, 0, 0);
   lv_obj_set_style_local_text_color(total_label, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_CYAN);
 
   for (uint8_t i = 0; i < zone_bar.size(); i++) {
@@ -89,9 +89,11 @@ HeartRateZone::~HeartRateZone() {
 
 void HeartRateZone::Refresh() {
   auto activity = heartRateController.Activity();
+  auto settings = heartRateController.hrzSettings();
   uint32_t total = 0;
 
   auto hundreths_of_hour = pdMS_TO_TICKS(10 * 60 * 60);
+  auto exercise_target = pdMS_TO_TICKS(settings.exerciseMsTarget);
 
   for (uint8_t i = 0; i < zone_bar.size(); i++) {
     total += activity.zoneTime[i];
